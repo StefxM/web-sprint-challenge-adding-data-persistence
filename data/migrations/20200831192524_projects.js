48fb8301e62b14c86828782922e0be6ba0283f14
stephanie mairena <stefyem7711@gmail.com>
2020-08-31 22:31:26 -0400

    postbugs

--- data/migrations/20200831192524_projects.js
+++ data/migrations/20200831192524_projects.js
@@ -2,10 +2,8 @@
 exports.up = function(knex) {
   return knex.schema
     .createTable('projects', table => {
-        table.increments()
-            .unique();
+        table.increments();
         table.string('project_name')
-            .unique()
             .notNullable();
         table.string('description')
         table.boolean('completed')
